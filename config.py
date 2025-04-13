04056fd7ff774b4fd151aa848d22bf907d6a444d
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-04-13 14:04:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+API_ID = 123456  # Replace with your own
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+SESSION_STRING = "your_userbot_session_string"  # From pyrogram Client (StringSession)
